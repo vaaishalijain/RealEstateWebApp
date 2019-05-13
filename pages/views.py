942a58ad7221077a352afed9fd7e9e4ff21cf177
vaaishalijain <vaishalij00@gmail.com>
2019-05-13 23:16:39 +0530

    added dynamic content to other pages

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from listings.models import Listing 
+from realtors.models import Realtor
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+  context = {
+    'listings' : listings
+  }
+  return render(request, 'pages/index.html', context)
     
 def about(request):
-    return render(request, 'pages/about.html')
+  realtors = Realtor.objects.order_by('-hire_date')
+  mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+  context = {
+    'realtors': realtors,
+    'mvp_realtors': mvp_realtor
+  }
+  return render(request, 'pages/about.html', context)
