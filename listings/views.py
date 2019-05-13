54551c5410612fd70a1c7df46dc08fe5079de610
vaaishalijain <vaishalij00@gmail.com>
2019-05-13 21:42:34 +0530

    Added models, connected to db, added media settings, completed admin setup, changed css of admin and dynamically displaying content for listings page

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def index(request):
-  return render(request, 'listings/listings.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+  
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
 
-def listing(request):
+  context = {
+    'listings': paged_listings
+  }
+  return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
   return render(request, 'listings/listing.html')
 
 def search(request):
--- realtors/models.py
+++ realtors/models.py
@@ -9,6 +9,6 @@ class Realtor(models.Model):
   phone = models.CharField(max_length=20)
   email = models.CharField(max_length=50)
   is_mvp = models.BooleanField(default=False)
-  hire_data = models.DateTimeField(default=datetime.now, blank=True)
+  hire_date = models.DateTimeField(default=datetime.now, blank=True)
   def __str__(self):
     return self.name
