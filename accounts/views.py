e62c7098cb8d1f96462579335ea96056d627c474
vaaishalijain <vaishalij00@gmail.com>
2019-05-17 01:43:15 +0530

    Completed dashboard functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 # Create your views here.
 
 def login(request):
@@ -60,4 +61,8 @@ def logout(request):
     return redirect('index')
 
 def dashboard(request):
-  return render(request, 'accounts/dashboard.html')
+  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+  context = {
+    'contacts' : user_contacts
+  }
+  return render(request, 'accounts/dashboard.html', context)
